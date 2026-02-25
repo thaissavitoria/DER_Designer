7e13dc4f93e87e3600d5f18ab42018982d2894d5
Thaissa Vitória <thaissavivi@gmail.com>
2026-02-24 23:19:58 -0300

    feat: add fit-to-view and Ctrl+F shortcut

--- src/viewer/DiagramView.cpp
+++ src/viewer/DiagramView.cpp
@@ -6,222 +6,272 @@
 #include <QtGui/QDragMoveEvent>
 #include <QtGui/QDropEvent>
 #include <QtGui/QWheelEvent>
+#include <QtGui/QKeyEvent>
 #include <QtWidgets/QApplication>
 
 //----------------------------------------------------------------------------------------------
 
 DiagramView::DiagramView(
-    DiagramScene* scene,
-    QWidget* parent
+  DiagramScene* scene,
+  QWidget* parent
 )
-    : QGraphicsView(scene, parent)
-    , m_currentZoom(DEFAULT_ZOOM)
+  : QGraphicsView(scene, parent)
+  , m_currentZoom(DEFAULT_ZOOM)
 {
-    setAcceptDrops(true);
-    setRenderHint(QPainter::Antialiasing);
-    setDragMode(QGraphicsView::RubberBandDrag);
-    setTransformationAnchor(QGraphicsView::AnchorUnderMouse);
-    setResizeAnchor(QGraphicsView::AnchorUnderMouse);
-    setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
-    setHorizontalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+  setAcceptDrops(true);
+  setRenderHint(QPainter::Antialiasing);
+  setDragMode(QGraphicsView::RubberBandDrag);
+  setTransformationAnchor(QGraphicsView::AnchorUnderMouse);
+  setResizeAnchor(QGraphicsView::AnchorUnderMouse);
+  setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+  setHorizontalScrollBarPolicy(Qt::ScrollBarAsNeeded);
 }
 
 //----------------------------------------------------------------------------------------------
 
 void DiagramView::dragEnterEvent(
-    QDragEnterEvent* event
+  QDragEnterEvent* event
 )
 {
-    if (event->mimeData()->hasFormat("application/x-element-type")) {
-        QString elementType = QString::fromUtf8(event->mimeData()->data("application/x-element-type"));
-        
-        if (isValidElementType(elementType)) {
-            event->acceptProposedAction();
-            return;
-        }
+  if (event->mimeData()->hasFormat("application/x-element-type")) {
+    QString elementType = QString::fromUtf8(event->mimeData()->data("application/x-element-type"));
+
+    if (isValidElementType(elementType)) {
+      event->acceptProposedAction();
+      return;
     }
-    
-    QGraphicsView::dragEnterEvent(event);
+  }
+
+  QGraphicsView::dragEnterEvent(event);
 }
 
 //----------------------------------------------------------------------------------------------
 
 void DiagramView::dragMoveEvent(
-    QDragMoveEvent* event
+  QDragMoveEvent* event
 )
 {
-    if (event->mimeData()->hasFormat("application/x-element-type")) {
-        QString elementType = QString::fromUtf8(event->mimeData()->data("application/x-element-type"));
-        
-        if (isValidElementType(elementType)) {
-            event->acceptProposedAction();
-            return;
-        }
+  if (event->mimeData()->hasFormat("application/x-element-type")) {
+    QString elementType = QString::fromUtf8(event->mimeData()->data("application/x-element-type"));
+
+    if (isValidElementType(elementType)) {
+      event->acceptProposedAction();
+      return;
     }
-    
-    QGraphicsView::dragMoveEvent(event);
+  }
+
+  QGraphicsView::dragMoveEvent(event);
 }
 
 //----------------------------------------------------------------------------------------------
 
 void DiagramView::dropEvent(
-    QDropEvent* event
+  QDropEvent* event
 )
 {
-    if (event->mimeData()->hasFormat("application/x-element-type")) {
-        QString elementType = QString::fromUtf8(event->mimeData()->data("application/x-element-type"));
-        
-        if (isValidElementType(elementType)) {
-            QPointF scenePos = mapToScene(event->pos());
-            emit elementDropped(elementType, scenePos);
-            event->acceptProposedAction();
-            return;
-        }
+  if (event->mimeData()->hasFormat("application/x-element-type")) {
+    QString elementType = QString::fromUtf8(event->mimeData()->data("application/x-element-type"));
+
+    if (isValidElementType(elementType)) {
+      QPointF scenePos = mapToScene(event->pos());
+      emit elementDropped(elementType, scenePos);
+      event->acceptProposedAction();
+      return;
     }
-    
-    QGraphicsView::dropEvent(event);
+  }
+
+  QGraphicsView::dropEvent(event);
 }
 
 //----------------------------------------------------------------------------------------------
 
 void DiagramView::wheelEvent(
-    QWheelEvent* event
+  QWheelEvent* event
 )
 {
-    if (QApplication::keyboardModifiers() & Qt::ControlModifier) {
-        const qreal scaleFactor = (event->angleDelta().y() > 0) ? ZOOM_STEP : (1.0 / ZOOM_STEP);
-        const QPointF centerPoint = mapToScene(event->position().toPoint());
-        
-        applyZoom(scaleFactor, centerPoint);
-        event->accept();
-    } else {
-        QGraphicsView::wheelEvent(event);
-    }
+  if (QApplication::keyboardModifiers() & Qt::ControlModifier) {
+    const qreal scaleFactor = (event->angleDelta().y() > 0) ? ZOOM_STEP : (1.0 / ZOOM_STEP);
+    const QPointF centerPoint = mapToScene(event->position().toPoint());
+
+    applyZoom(scaleFactor, centerPoint);
+    event->accept();
+  }
+  else {
+    QGraphicsView::wheelEvent(event);
+  }
+}
+
+//----------------------------------------------------------------------------------------------
+
+void DiagramView::keyPressEvent(
+  QKeyEvent* event
+)
+{
+  if (event->modifiers() & Qt::ControlModifier && event->key() == Qt::Key_F) {
+    fitToView();
+    event->accept();
+  }
+  else {
+    QGraphicsView::keyPressEvent(event);
+  }
 }
 
 //----------------------------------------------------------------------------------------------
 
 void DiagramView::drawBackground(
-    QPainter* painter,
-    const QRectF& rect
+  QPainter* painter,
+  const QRectF& rect
 )
 {
-    painter->fillRect(rect, QColor(255, 255, 255));
+  painter->fillRect(rect, QColor(255, 255, 255));
 
-    if (m_gridVisible) {
-        drawGrid(painter, rect);
-    }
+  if (m_gridVisible) {
+    drawGrid(painter, rect);
+  }
 }
 
 //----------------------------------------------------------------------------------------------
 
 void DiagramView::drawGrid(
-    QPainter* painter,
-    const QRectF& rect
+  QPainter* painter,
+  const QRectF& rect
 )
 {
-    painter->save();
+  painter->save();
 
-    QPen gridPen(QColor(220, 220, 220, 200), 1, Qt::SolidLine);
-    painter->setPen(gridPen);
+  QPen gridPen(QColor(220, 220, 220, 200), 1, Qt::SolidLine);
+  painter->setPen(gridPen);
 
-    qreal left = static_cast<int>(rect.left() / GRID_SIZE) * GRID_SIZE;
-    qreal top = static_cast<int>(rect.top() / GRID_SIZE) * GRID_SIZE;
+  qreal left = static_cast<int>(rect.left() / GRID_SIZE) * GRID_SIZE;
+  qreal top = static_cast<int>(rect.top() / GRID_SIZE) * GRID_SIZE;
 
-    for (qreal x = left; x <= rect.right(); x += GRID_SIZE) {
-        painter->drawLine(QPointF(x, rect.top()), QPointF(x, rect.bottom()));
-    }
+  for (qreal x = left; x <= rect.right(); x += GRID_SIZE) {
+    painter->drawLine(QPointF(x, rect.top()), QPointF(x, rect.bottom()));
+  }
 
-    for (qreal y = top; y <= rect.bottom(); y += GRID_SIZE) {
-        painter->drawLine(QPointF(rect.left(), y), QPointF(rect.right(), y));
-    }
+  for (qreal y = top; y <= rect.bottom(); y += GRID_SIZE) {
+    painter->drawLine(QPointF(rect.left(), y), QPointF(rect.right(), y));
+  }
 
-    painter->restore();
+  painter->restore();
 }
 
 //----------------------------------------------------------------------------------------------
 
 void DiagramView::zoomIn()
 {
-    applyZoom(ZOOM_STEP);
+  applyZoom(ZOOM_STEP);
 }
 
 //----------------------------------------------------------------------------------------------
 
 void DiagramView::zoomOut()
 {
-    applyZoom(1.0 / ZOOM_STEP);
+  applyZoom(1.0 / ZOOM_STEP);
 }
 
 //----------------------------------------------------------------------------------------------
 
 void DiagramView::resetZoom()
 {
-    resetTransform();
-    m_currentZoom = DEFAULT_ZOOM;
+  resetTransform();
+  m_currentZoom = DEFAULT_ZOOM;
+}
+
+//----------------------------------------------------------------------------------------------
+
+void DiagramView::fitToView()
+{
+  if (!scene()) {
+    return;
+  }
+
+  QRectF sceneRect = scene()->itemsBoundingRect();
+
+  if (sceneRect.isEmpty()) {
+    return;
+  }
+
+  const qreal margin = 50.0;
+  sceneRect.adjust(-margin, -margin, margin, margin);
+
+  resetTransform();
+  fitInView(sceneRect, Qt::KeepAspectRatio);
+
+  QTransform transform = this->transform();
+  qreal scaleX = transform.m11();
+  qreal scaleY = transform.m22();
+  m_currentZoom = qMin(scaleX, scaleY);
+
+  if (m_currentZoom < MIN_ZOOM) {
+    setZoomLevel(MIN_ZOOM);
+  }
+  else if (m_currentZoom > MAX_ZOOM) {
+    setZoomLevel(MAX_ZOOM);
+  }
 }
 
 //----------------------------------------------------------------------------------------------
 
 void DiagramView::setZoomLevel(
-    qreal zoomLevel
+  qreal zoomLevel
 )
 {
-    if (zoomLevel < MIN_ZOOM || zoomLevel > MAX_ZOOM) {
-        return;
-    }
-    
-    resetTransform();
-    scale(zoomLevel, zoomLevel);
-    m_currentZoom = zoomLevel;
+  if (zoomLevel < MIN_ZOOM || zoomLevel > MAX_ZOOM) {
+    return;
+  }
+
+  resetTransform();
+  scale(zoomLevel, zoomLevel);
+  m_currentZoom = zoomLevel;
 }
 
 //----------------------------------------------------------------------------------------------
 
 qreal DiagramView::getZoomLevel() const
 {
-    return m_currentZoom;
+  return m_currentZoom;
 }
 
 //----------------------------------------------------------------------------------------------
 
 void DiagramView::applyZoom(
-    qreal scaleFactor,
-    const QPointF& centerPoint
+  qreal scaleFactor,
+  const QPointF& centerPoint
 )
 {
-    qreal newZoom = m_currentZoom * scaleFactor;
-    
-    if (newZoom < MIN_ZOOM || newZoom > MAX_ZOOM) {
-        return;
-    }
-    
-    if (!centerPoint.isNull()) {
-        centerOn(centerPoint);
-    }
-    
-    scale(scaleFactor, scaleFactor);
-    m_currentZoom = newZoom;
+  qreal newZoom = m_currentZoom * scaleFactor;
+
+  if (newZoom < MIN_ZOOM || newZoom > MAX_ZOOM) {
+    return;
+  }
+
+  if (!centerPoint.isNull()) {
+    centerOn(centerPoint);
+  }
+
+  scale(scaleFactor, scaleFactor);
+  m_currentZoom = newZoom;
 }
 
 //----------------------------------------------------------------------------------------------
 
 bool DiagramView::isValidElementType(
-    const QString& elementType
+  const QString& elementType
 ) const
 {
-    QStringList validTypes = {
-        "Entity",
-        "WeakEntity", 
-        "Attribute",
-        "KeyAttribute",
-        "DerivedAttribute",
-        "MultivaluedAttribute",
-        "Relationship",
-        "IdentifyingRelationship"
-    };
-    
-    return validTypes.contains(elementType);
+  QStringList validTypes = {
+      "Entity",
+      "WeakEntity",
+      "Attribute",
+      "KeyAttribute",
+      "DerivedAttribute",
+      "MultivaluedAttribute",
+      "Relationship",
+      "IdentifyingRelationship"
+  };
+
+  return validTypes.contains(elementType);
 }
 
 //----------------------------------------------------------------------------------------------
--- src/viewer/DiagramView.h
+++ src/viewer/DiagramView.h
@@ -6,80 +6,86 @@
 #include <QtGui/QDragMoveEvent>
 #include <QtGui/QDropEvent>
 #include <QtGui/QWheelEvent>
+#include <QtGui/QKeyEvent>
 
 class DiagramScene;
 
 class DiagramView : public QGraphicsView
 {
-    Q_OBJECT
+  Q_OBJECT
 
 public:
-    explicit DiagramView(
-        DiagramScene* scene,
-        QWidget* parent = nullptr
-    );
-    virtual ~DiagramView() = default;
+  explicit DiagramView(
+    DiagramScene* scene,
+    QWidget* parent = nullptr
+  );
+  virtual ~DiagramView() = default;
 
-    void zoomIn();
-    void zoomOut();
-    void resetZoom();
-    void setZoomLevel(
-        qreal zoomLevel
-    );
-    qreal getZoomLevel() const;
+  void zoomIn();
+  void zoomOut();
+  void resetZoom();
+  void fitToView();
+  void setZoomLevel(
+    qreal zoomLevel
+  );
+  qreal getZoomLevel() const;
 
 protected:
-    void dragEnterEvent(
-        QDragEnterEvent* event
-    ) override;
+  void dragEnterEvent(
+    QDragEnterEvent* event
+  ) override;
 
-    void dragMoveEvent(
-        QDragMoveEvent* event
-    ) override;
+  void dragMoveEvent(
+    QDragMoveEvent* event
+  ) override;
 
-    void dropEvent(
-        QDropEvent* event
-    ) override;
+  void dropEvent(
+    QDropEvent* event
+  ) override;
 
-    void wheelEvent(
-        QWheelEvent* event
-    ) override;
+  void wheelEvent(
+    QWheelEvent* event
+  ) override;
 
-    void drawBackground(
-        QPainter* painter,
-        const QRectF& rect
-    ) override;
+  void keyPressEvent(
+    QKeyEvent* event
+  ) override;
+
+  void drawBackground(
+    QPainter* painter,
+    const QRectF& rect
+  ) override;
 
 signals:
-    void elementDropped(
-        const QString& elementType,
-        const QPointF& position
-    );
+  void elementDropped(
+    const QString& elementType,
+    const QPointF& position
+  );
 
 private:
-    bool isValidElementType(
-        const QString& elementType
-    ) const;
+  bool isValidElementType(
+    const QString& elementType
+  ) const;
 
-    void applyZoom(
-        qreal scaleFactor,
-        const QPointF& centerPoint = QPointF()
-    );
+  void applyZoom(
+    qreal scaleFactor,
+    const QPointF& centerPoint = QPointF()
+  );
 
-    void drawGrid(
-        QPainter* painter,
-        const QRectF& rect
-    );
+  void drawGrid(
+    QPainter* painter,
+    const QRectF& rect
+  );
 
-    const qreal MIN_ZOOM = 0.1;
-    const qreal MAX_ZOOM = 5.0;
-    const qreal ZOOM_STEP = 1.25;
-    const qreal DEFAULT_ZOOM = 1.0;
-	const qreal GRID_SIZE = 30.0;
+  const qreal MIN_ZOOM = 0.1;
+  const qreal MAX_ZOOM = 5.0;
+  const qreal ZOOM_STEP = 1.25;
+  const qreal DEFAULT_ZOOM = 1.0;
+  const qreal GRID_SIZE = 30.0;
 
-    qreal m_currentZoom;
+  qreal m_currentZoom;
 
-	bool m_gridVisible = true; /*implementar grid on off depois?*/
+  bool m_gridVisible = true; /*implementar grid on off depois?*/
 };
 
 #endif // DIAGRAMVIEW_H
